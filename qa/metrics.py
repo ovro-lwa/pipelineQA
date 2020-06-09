b1c5dd060c8a52e698f27ff1208c8c6f8ef5ca93
Casey Law <caseyjlaw@gmail.com>
2020-06-09 11:41:47 -0700

    builds properly. metrics channel flag fract function

--- qa/metrics.py
+++ qa/metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.ndimage import filters
 import casatools
+import os.path
 
 tb = casatools.table()
 
@@ -8,10 +9,31 @@ def flagfrac(msfile):
     """ Get fraction of visibilities (crosscorr) that are flagged
     """
 
-    tb.open(msfile, nomodify=False)
+    if not os.path.exists(msfile):
+        logger.warn('File {0} not found'.format(msfile))
+        return None
+
+    tb.open(msfile, nomodify=True)
     tcross  = tb.query('ANTENNA1!=ANTENNA2')
     flagcol = tcross.getcol('FLAG')
     flagarr = np.rollaxis(flagcol[0,:,:] | flagcol[1,:,:] | flagcol[2,:,:] | flagcol[3,:,:], 1)
     frac = np.count_nonzero(flagarr)/flagarr.size
+
+    return frac
+
+
+def flagfrac_chans(msfile):
+    """ Get fraction of visibilities (crosscorr) that are flagged per channel
+    """
+
+    if not os.path.exists(msfile):
+        logger.warn('File {0} not found'.format(msfile))
+        return None
+
+    tb.open(msfile, nomodify=True)
+    tcross  = tb.query('ANTENNA1!=ANTENNA2')
+    flagcol = tcross.getcol('FLAG')
+    flagarr = np.rollaxis(flagcol[0,:,:] | flagcol[1,:,:] | flagcol[2,:,:] | flagcol[3,:,:], 1)
+    frac = [np.count_nonzero(flagarr[:,i])/flagarr[:,i].size for i in range(len(flagarr[0]))]
 
     return frac
